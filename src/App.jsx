d7b568dcd8c286f4ba610f6b2f91b384394139e3
Elias Souza <97631394+eliasnsz@users.noreply.github.com>
2022-08-31 18:00:48 -0300

    Estruturação do Projeto
    Neste primeiro commit realizei a criação do projeto utilizando Vite para gerar a base em react. Também concluí a criação do Header do website.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.scss'
+import { Header } from './components/Header/Header'
+
+export default function App() {
+
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  )
+}
+
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import './Header.scss'
+
+export function Header() {
+  return (
+    <>
+      <header id="main-header">
+        <section className="main-header-content">
+          
+          <div className="main-header-logo">
+            <svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 133.46 118.11"><defs><style>.cls-1;</style></defs><polygon class="cls-1" points="84.13 0 133.46 0 133.46 118.11 84.13 0"/><polygon class="cls-1" points="49.37 0 0 0 0 118.11 49.37 0"/><polygon class="cls-1" points="66.75 43.53 98.18 118.11 77.58 118.11 68.18 94.36 45.18 94.36 66.75 43.53"/></svg>
+            <span className='main-header-logo-title'>Adobe</span>
+          </div>
+
+          <div className="header-right-section">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" focusable="false">
+              <path d="M14 2A8 8 0 0 0 7.4 14.5L2.4 19.4a1.5 1.5 0 0 0 2.1 2.1L9.5 16.6A8 8 0 1 0 14 2Zm0 14.1A6.1 6.1 0 1 1 20.1 10 6.1 6.1 0 0 1 14 16.1Z"></path>
+          </svg>
+
+          <span>Fazer logon</span>
+          </div>
+
+        </section>
+      </header>
+    </>
+  )
+}
